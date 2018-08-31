3ef86cef7338055fd5a494d7134dedb0ad6162b9
Jonatan Berggren <JBerggren@users.noreply.github.com>
2018-08-31 14:43:44 +0200

    Save users name Layout

--- Chat/Client/js/chat.js
+++ Chat/Client/js/chat.js
@@ -1,10 +1,10 @@
 "use strict";
 
-var connection = new signalR.HubConnectionBuilder().withUrl("https://appendhouse.contentowassum.se/chat/chatHub").build();
+var connection = new signalR.HubConnectionBuilder().withUrl("https://appendhouse.contentowassum.se/chat/backend/chatHub").build();
 
 connection.on("ReceiveMessage", function (user, message) {
     var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
-    var encodedMsg = user + " says " + msg;
+    var encodedMsg = user + ": " + msg;
     var li = document.createElement("li");
     li.textContent = encodedMsg;
     document.getElementById("messagesList").appendChild(li);
@@ -21,4 +21,15 @@ document.getElementById("sendButton").addEventListener("click", function (event)
         return console.error(err.toString());
     });
     event.preventDefault();
+});
+
+var userInput = document.getElementById("userInput");
+var messageInput = document.getElementById("messageInput");
+
+if(window.localStorage){
+    userInput.value = localStorage.getItem("username");
+}
+
+userInput.addEventListener("change",function(event){
+    window.localStorage.setItem("username",userInput.value);
 });
