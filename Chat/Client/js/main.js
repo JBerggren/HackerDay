c94db7c54d7fe37063a24e16257c7f92105d67a9
Jonatan Berggren <JBerggren@users.noreply.github.com>
2018-08-31 15:51:08 +0200

    Added as PWA

---
+++ Chat/Client/js/main.js
@@ -0,0 +1,45 @@
+//Install Service worker (right now just to fill the criterias for auto suggest Web app install https://developers.google.com/web/fundamentals/app-install-banners/#criteria)
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function () {
+        navigator.serviceWorker.register('sw.js').then(function (registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function (err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+}
+
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+
+    showAddBtn(e);
+});
+
+function showAddBtn(event) {
+    var deferredPrompt = event;
+    //Right now we just show it straight away
+    document.getElementById("addToStartScreen").style.display = "block";
+
+    var btnAdd = document.getElementById("btnAdd");
+    btnAdd.addEventListener('click', (e) => {
+        // hide our user interface that shows our A2HS button
+        btnAdd.style.display = 'none';
+        // Show the prompt
+        deferredPrompt.prompt();
+        // Wait for the user to respond to the prompt
+        deferredPrompt.userChoice
+            .then((choiceResult) => {
+                if (choiceResult.outcome === 'accepted') {
+                    console.log('User accepted the A2HS prompt');
+                } else {
+                    console.log('User dismissed the A2HS prompt');
+                }
+                deferredPrompt = null;
+            });
+    });
+}
